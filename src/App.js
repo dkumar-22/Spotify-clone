e87ebd055423a8e2ef4f318980489afb8735ebb2
Dhruv Kumar <cool.dhruv225@gmail.com>
2020-12-30 18:10:13 +0530

    Designed the Login Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
 import './App.css';
+import Login from "./Login";
+
 
 function App() {
-  return (<div className="App"></div>);
+  return (<div className="App">
+    <Login />
+  </div>);
 }
 
 export default App;
+
+//7dfaad7a61304aa2ace36eb7e6bc32df
